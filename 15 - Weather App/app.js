a863a53ffeb055826d503175ba9744c634d49c7d
tahacagrimen <tahacagrimen2@gmail.com>
2022-06-17 04:46:55 +0300

    weather app final

--- 15 - Weather App/app.js
+++ 15 - Weather App/app.js
@@ -5,12 +5,53 @@ let weather = {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
     const { name } = data;
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    const { description } = data.weather[0];
+    const { icon, description } = data.weather[0];
+
+    console.log(name, temp, humidity, speed, description, icon);
+
+    document.querySelector(".card").classList.replace("h-30", "h-80");
+
+    document.querySelector(".city").innerHTML = name;
+    document.querySelector(".temp").innerHTML = temp + " &deg;C";
+    document.querySelector(".humidity").innerHTML = "Humidity : " + humidity;
+    document.querySelector(".wind").innerHTML = "Wind Speed : " + speed;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector(".desc").innerHTML = description;
+    document.querySelector(".weather").classList.remove("loading");
+
+    document.querySelector(".search-bar").value = "";
+
+    if (data.weather[0].icon[2] == "n") {
+      document
+        .querySelector(".weather")
+        .classList.replace("bg-blue-400/75", "bg-gray-600/75");
+    } else if (data.weather[0].icon[2] == "d") {
+      document
+        .querySelector(".weather")
+        .classList.replace("bg-gray-600/75", "bg-blue-400/75");
+    }
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
+
+document.querySelector(".search button").addEventListener("click", (e) => {
+  e.preventDefault();
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    weather.search();
+  }
+});
